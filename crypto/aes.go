9c0eee3d41cce8e8580771e2f459a5b7f3afa1cb
yangfan <yangfan@qingsongchou.com>
2020-12-02 11:25:31 +0800

    fix bug

--- crypto/aes.go
+++ crypto/aes.go
@@ -10,69 +10,72 @@ import (
 	"errors"
 )
 
-func AesEncrypt(plaintext, key, iv string) (string, error) {
-	block, err := aes.NewCipher([]byte(key))
+func AesEncrypt(plaintext, key, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	plaintext = pkcs7padding(plaintext, block.BlockSize())
-	blockModel := cipher.NewCBCEncrypter(block, []byte(iv))
+	blockModel := cipher.NewCBCEncrypter(block, iv)
 	ciphertext := make([]byte, len(plaintext))
-	blockModel.CryptBlocks(ciphertext, []byte(plaintext))
-	return string(ciphertext), nil
+	blockModel.CryptBlocks(ciphertext, plaintext)
+	return ciphertext, nil
 }
 
-func AesDecrypt(ciphertext, key, iv string) (string, error) {
-	block, err := aes.NewCipher([]byte(key))
+func AesDecrypt(ciphertext, key, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	blockMode := cipher.NewCBCDecrypter(block, []byte(iv))
-	originText := make([]byte, len(ciphertext))
-	blockMode.CryptBlocks(originText, []byte(ciphertext))
-	return pkcs7unpadding(string(originText)), nil
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	origintext := make([]byte, len(ciphertext))
+	blockMode.CryptBlocks(origintext, ciphertext)
+	origintext = pkcs7unpadding(origintext)
+	return origintext, nil
 }
 
-func AesQuickEncrypt(plaintext, key string) (string, error) {
-	block, err := aes.NewCipher([]byte(key))
+func AesQuickEncrypt(plaintext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	iv := make([]byte, block.BlockSize())
-	_, _ = rand.Read(iv)
-	ciphertext, _ := AesEncrypt(plaintext, key, string(iv))
-	dataMap := map[string]string{"iv": string(iv), "value": ciphertext}
-	data, err := json.Marshal(dataMap)
+	rand.Read(iv)
+	ciphertext, _ := AesEncrypt(plaintext, key, iv)
+	datamap := map[string][]byte{"iv": iv, "value": ciphertext}
+	data, err := json.Marshal(datamap)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return base64.StdEncoding.EncodeToString(data), nil
+	rst := base64.StdEncoding.EncodeToString(data)
+	return []byte(rst), nil
 }
 
-func AesQuickDecrypt(cipher, key string) (string, error) {
+func AesQuickDecrypt(cipher, key []byte) ([]byte, error) {
 	ciphertext, err := base64.StdEncoding.DecodeString(string(cipher))
 	if err != nil {
-		return "", errors.New("BASE64 解码错误")
+		return nil, errors.New("BASE64 解码错误")
 	}
 	x := make(map[string]string)
-	_ = json.Unmarshal(ciphertext, &x)
+	json.Unmarshal(ciphertext, &x)
 	iv, ok := x["iv"]
 	if !ok {
-		return "", errors.New("无法确定iv")
+		return nil, errors.New("无法确定iv")
 	}
 	value, _ := x["value"]
 	ivDecode, _ := base64.StdEncoding.DecodeString(iv)
 	vaDecode, _ := base64.StdEncoding.DecodeString(value)
-	return AesDecrypt(string(vaDecode), key, string(ivDecode))
+	return AesDecrypt(vaDecode, key, ivDecode)
 }
 
-func pkcs7padding(ciphertext string, blockSize int) string {
+
+func pkcs7padding(ciphertext []byte, blockSize int) []byte {
 	padding := blockSize - len(ciphertext)%blockSize
 	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	return ciphertext + string(padText)
+	return append(ciphertext, padText...)
 }
 
-func pkcs7unpadding(plaintext string) string {
+func pkcs7unpadding(plaintext []byte) []byte {
 	length := len(plaintext)
 	padding := int(plaintext[length-1])
 	return plaintext[:(length - padding)]
