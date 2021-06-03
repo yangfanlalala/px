372981dc60777ce1a528d1f156d554a22acb54ae
yangfan <yangfan.lalala@hotmail.com>
2021-06-04 00:07:52 +0800

    add function

--- crypto/rsa.go
+++ crypto/rsa.go
@@ -7,20 +7,40 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"errors"
-	"fmt"
 )
-
-func RSAPKCS1Encrypt(plain, pk string) {
-
+func RSAPKCS1Encrypt(plain, pk string) (string, error){
+	block, _ := pem.Decode([]byte(pk))
+	if block == nil {
+		return "", errors.New("block is empty")
+	}
+	x, err := x509.ParsePKCS1PublicKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+	rst, err := rsa.EncryptPKCS1v15(rand.Reader, x, []byte(plain))
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(rst), nil
 }
 
-func RSAPKCS1Decrypt(cipher, pk string) {
-
+func RSAPKCS1Decrypt(cipher, pk string) ([]byte, error){
+	block, _ := pem.Decode([]byte(pk))
+	if block == nil {
+		return nil, errors.New("block is empty")
+	}
+	x, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	ct, _ := base64.StdEncoding.DecodeString(cipher)
+	rst, err := rsa.DecryptPKCS1v15(rand.Reader, x, ct)
+	if err != nil {
+		return nil, err
+	}
+	return rst, nil
 }
 
-func RSAPKCS8Encrypt(plain, pk string) {
-
-}
 
 func RSAPKCS8Decrypt(cipher, pk string) ([]byte, error) {
 	block, _ := pem.Decode([]byte(pk))
@@ -29,13 +49,11 @@ func RSAPKCS8Decrypt(cipher, pk string) ([]byte, error) {
 	}
 	x, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if err != nil {
-		fmt.Println("$2")
 		return nil, err
 	}
 	ct, _ := base64.StdEncoding.DecodeString(cipher)
 	rst, err := rsa.DecryptPKCS1v15(rand.Reader, x.(*rsa.PrivateKey), ct)
 	if err != nil {
-		fmt.Println("$1")
 		return nil, err
 	}
 	return rst, nil
